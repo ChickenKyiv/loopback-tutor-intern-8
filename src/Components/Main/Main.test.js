0d386abb14fc94ad54e1eafbed358624ad333a3e
chauhannishith <chauhannis123ith@gmail.com>
2018-05-24 18:26:01 +0530

    still not working

--- src/Components/Main/Main.test.js
+++ src/Components/Main/Main.test.js
@@ -1,8 +1,20 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router';
+import Adapter from 'enzyme-adapter-react-15';
+import { mount, configure } from 'enzyme';
+import Main from './Main';
+import Home from '../../pages/Home';
+import Page404 from '../../pages/Page404';
 
+configure({ adapter: new Adapter() });
 
-// it('Main renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-// });
+it('Invalid page redirect to 404', () => {
+  const wrapper = mount(
+  	<MemoryRouter initialEntries={['/random']}>
+  	  <Main />
+  	</MemoryRouter>
+  	);
+  	expect(wrapper.find(Home)).toHaveLength(0);
+  	expect(wrapper.find(NotFoundPage)).toHaveLength(1);
+});
